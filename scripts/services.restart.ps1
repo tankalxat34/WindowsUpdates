15eb6e1854959bc804285239c62fbafef28d9b87
Alexander <tankalxat34@gmail.com>
2024-09-08 00:30:22 +0300

    Added scripts to restart and stop Windows updaters

---
+++ scripts/services.restart.ps1
@@ -0,0 +1,3 @@
+Restart-Service -Name wuauserv
+Restart-Service -Name InstallService
+Restart-Service -Name UsoSvc
---
+++ scripts/services.stop.ps1
@@ -0,0 +1,3 @@
+Stop-Service -Name wuauserv -Force
+Stop-Service -Name InstallService -Force
+Stop-Service -Name UsoSvc -Force
